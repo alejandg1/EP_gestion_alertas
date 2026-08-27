06336badc84c49b6eeeb73e800b4f4311cbfd2d6
Jose Alejandro Gomez Paredes <jose.gomez@seguraep.gob.ec>
2026-08-27 08:47:34 -0500

    fix uri

--- src/config/database.js
+++ src/config/database.js
@@ -3,12 +3,17 @@ const logger = require('./logger');
 
 const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI;
+    const uri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
     const dbName = process.env.DB_NAME || 'sala_situacional';
     
+    if (!process.env.MONGODB_URI) {
+      logger.warn(`Variable MONGODB_URI no definida en .env. Usando por defecto: ${uri}`);
+    }
+
     await mongoose.connect(uri, {
       dbName: dbName,
     });
+
     
     logger.info(`MongoDB conectado exitosamente a la BD: ${dbName}`);
   } catch (error) {
